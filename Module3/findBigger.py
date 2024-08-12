7ed614394d08ef4d2bd57c8b9914a5a7f570d626
Guilherme <guilhermeplays03@gmail.com>
2024-08-12 20:28:12 -0300

    starting module3, if, elif, else

---
+++ Module3/findBigger.py
@@ -0,0 +1,15 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+# assumir que o maior numero é o primeiro, breve iremos verificar
+# isso irá nos facilitar
+largest_number = number1
+
+if number2 > largest_number:
+    largest_number = number2
+
+if number3 > largest_number:
+    largest_number = number3
+
+print("The largest number is:", largest_number)

ebaf23d6cacbb96f3f30db3383ca3df3465d3ef9
Guilherme <guilhermeplays03@gmail.com>
2024-08-13 19:18:56 -0300

    loop while

---
+++ Module3/loop_while.py
@@ -0,0 +1,20 @@
+secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+number = int(input("Enter a number: "))
+
+while number != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    number = int(input("Try again newbie!!: "))
+
+print("Well done, muggle! You are free now.")
